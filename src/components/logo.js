20cb49d3c09240c09268124aa924386606c507a8
frantchessico <tchessico@live.com>
2022-02-20 15:22:51 -0800

    add logo image

--- src/components/logo.js
+++ src/components/logo.js
@@ -12,7 +12,7 @@ export default function Logo({ white }) {
         variant: 'links.logo',
       }}
     >
-      <h1 style={{color: "#000", fontWeight: "bold", fontFamily: "monospace"}}>Francisco Inoque</h1>
+      <h1 style={{color: "#000", fontWeight: "bold", fontFamily: "monospace", margin: 0}}>Francisco Inoque</h1>
     </Link>
   );
 }
