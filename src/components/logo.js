a36850590e251618121af42409c44256de2ead0a
frantchessico <tchessico@live.com>
2022-02-20 09:23:56 -0800

    add title

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,8 +1,8 @@
 /** @jsx jsx */
 import { jsx, Image } from 'theme-ui';
 import { Link } from 'components/link';
-import logo from 'assets/images/logo.svg';
-// import logoWhite from 'assets/images/white.png';
+import logo from 'assets/images/logo.png';
+import logoWhite from 'assets/images/logo-white.png';
 
 export default function Logo({ white }) {
   return (
@@ -12,7 +12,7 @@ export default function Logo({ white }) {
         variant: 'links.logo',
       }}
     >
-      {white ? <h2 style={{color: '#fff', fontWeight: 'bold', fontFamily: 'monospace'}}>FJ. </h2> : <h2 style={{margin: 0, color: '#000', fontWeight: 'bold', fontFamily: 'monospace'}}>Francisco Inoque</h2>} 
+      <Image src={white ? logoWhite : logo} alt="startup landing logo" />
     </Link>
   );
 }
