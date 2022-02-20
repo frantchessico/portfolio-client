f26caa3401c0c36c9423652e008b787eed6497c7
frantchessico <tchessico@live.com>
2022-02-20 08:48:20 -0800

    add icons dev

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,7 +2,7 @@
 import { jsx, Image } from 'theme-ui';
 import { Link } from 'components/link';
 import logo from 'assets/images/logo.svg';
-import logoWhite from 'assets/images/white.svg';
+// import logoWhite from 'assets/images/white.png';
 
 export default function Logo({ white }) {
   return (
@@ -12,7 +12,7 @@ export default function Logo({ white }) {
         variant: 'links.logo',
       }}
     >
-      <Image src={white ? logoWhite : logo} alt="startup landing logo" />
+      {white ? <h2 style={{color: '#fff'}}>Francisco Inoque</h2> : <h2>Francisco Inoque</h2>} 
     </Link>
   );
 }
